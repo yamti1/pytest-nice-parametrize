e1c247a8920607e7b4db2a9b3b0e6bfa3371f7ee
yam t <yam.tirosh@gmail.com>
2020-09-05 13:41:39 +0300

    Write setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+
+def get_requirements():
+    with open("requirements.txt", "r") as f:
+        return list(f)
+
+
+def get_description():
+    with open("README.md", 'r') as f:
+        return f.read()
+
+
+setuptools.setup(
+    name="nice-parametrize",
+    version="1.0.0",
+    author="Yam Tirosh",
+    author_email="yam.tirosh@gmail.com",
+    description="A small snippet for nicer PyTest's Parametrize",
+    long_description=get_description(),
+    py_modules=["nice_parametrize"],
+    install_requires=get_requirements(),
+    keywords="pytest parametrize ids",
+    classifiers=["Operating System :: OS Independent"],
+)

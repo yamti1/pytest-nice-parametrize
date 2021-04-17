cb9257c7df87e716755079ffab78737a83adf99f
yam t <yam.tirosh@gmail.com>
2021-04-17 12:49:27 +0300

    Publish ready

--- setup.py
+++ setup.py
@@ -14,12 +14,20 @@ def get_description():
 setuptools.setup(
     name="pytest-nice-parametrize",
     version="1.0.0",
+    license='MIT',
     author="Yam Tirosh",
     author_email="yam.tirosh@gmail.com",
     description="A small snippet for nicer PyTest's Parametrize",
+    download_url="https://github.com/yamti1/pytest-nice-parametrize/archive/refs/tags/1.0.0.tar.gz",
     long_description=get_description(),
     py_modules=["nice_parametrize"],
     install_requires=get_requirements(),
     keywords="pytest parametrize ids",
-    classifiers=["Operating System :: OS Independent"],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.7',
+    ],
 )
